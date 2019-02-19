2310697ef22604a6f5e76b4ca13568b4354b41e0
Danny McAllister <danny.mcallister@centre.edu>
2019-02-19 15:25:29 -0500

    small

--- scripts.js
+++ scripts.js
@@ -4,11 +4,12 @@ var list = function(){
   var animals = ["dog","panda","penguin", "polar bear", "cat","lion"];
   var ul = document.createElement('ul');
 
-  for(var i=0;i<animals.length;i++){
+  animals.forEach(function(d){
     var li = document.createElement("li");
-    li.innerHTML = animals[i];
+    li.innerHTML = d;
     ul.appendChild(li);
-  }
+  })
+
   var heading = document.getElementById("top");
   heading.after(ul);
 }
